e60268797a58d7ec030dfc56a88a5cf2a66e1067
Yesy Magana <yesy.magana@yahoo.com>
2022-05-22 13:49:27 -0700

    day1 and day2 changes

---
+++ day2/js/main.js
@@ -0,0 +1,27 @@
+document.querySelector('button').innerHTML = 'Click Me!'
+
+// prepare a kid for school or life
+
+// 1 how to say no
+false; //no
+
+// 2 how to say yes
+true;
+
+// numbers
+0;
+1;
+2;
+3;
+
+// alphabet
+'abc';
+FirstLettersOfTheAlphabet = 'abc';
+'yesenia';
+MyFirstName = 'yesenia';
+
+// Data Types
+// 1. string
+'abc';
+"yesenia";
+`unique power`;
